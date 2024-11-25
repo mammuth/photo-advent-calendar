7b9e37bc77e5c93e3d29a7a65239623f83caf221
Max Muth <kontakt@maxi-muth.de>
2024-11-25 11:25:18 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import copy from 'rollup-plugin-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    copy({
+      targets: [
+        { src: 'src/assets/calendars/**/*', dest: 'dist/assets/calendars' },
+      ],
+      verbose: true,
+    }),
   ],
   resolve: {
     alias: {
