f529a404c6874163627195c679e19528673886a6
Max Muth <kontakt@maxi-muth.de>
2024-11-25 11:31:21 +0100

    Revert "fix build"
    This reverts commit 7b9e37bc77e5c93e3d29a7a65239623f83caf221.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,5 +4,4 @@
 <template>
   <h1>Photo Advent Calendar</h1>
   <p>You shouldn't have landed here. Please ask for the correct link again.</p>
-  <p>If you're looking for a <b>demo</b> of this application, click <a href="/calendars/demo?allowAll&showAll">here.</a></p>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import copy from 'rollup-plugin-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,12 +11,6 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
-    copy({
-      targets: [
-        { src: 'src/assets/calendars/**/*', dest: 'dist/assets/calendars' },
-      ],
-      verbose: true,
-    }),
   ],
   resolve: {
     alias: {
