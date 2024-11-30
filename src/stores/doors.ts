dc76489cb5c914fa39be283aa18e650658ea644b
Max Muth <kontakt@maxi-muth.de>
2024-11-30 14:50:36 +0100

    fix: various reactivity and style fixes (#3)

--- src/stores/doors.ts
+++ src/stores/doors.ts
@@ -1,25 +1,15 @@
-import { useRoute } from "vue-router";
 import { defineStore } from "pinia";
-import { computed } from "vue";
 
 export const useDoorsStore = defineStore("openedDoors", {
-  state: () => {
-    const route = useRoute();
-    const allowAll = computed(() => route.query.allowAll !== undefined);
-    const showAll = computed(() => route.query.showAll !== undefined);
-
-    return {
-      persistedOpenedDoors: [] as number[],
-      doorsOrder: [] as number[],
-      allowAll: allowAll,
-      showAll: showAll,
-    };
-  },
+  state: () => ({
+    persistedOpenedDoors: [] as number[],
+    doorsOrder: [] as number[],
+  }),
   actions: {
-    openDoor(day: number) {
+    openDoor(day: number, allowAll: boolean) {
       const currentDay = new Date().getDate();
 
-      if (day > currentDay && !this.allowAll) {
+      if (day > currentDay && !allowAll) {
         window.alert(`It's not time to open door ${day} yet!`);
         return;
       }
@@ -39,13 +29,9 @@ export const useDoorsStore = defineStore("openedDoors", {
     },
   },
   getters: {
-    openedDoors(state) {
-      if (state.showAll) {
-        const allDoors = Array.from({ length: 24 }, (_, i) => i + 1);
-        return allDoors;
-      } else {
-        return state.persistedOpenedDoors;
-      }
+    openedDoors: (state) => (showAll: boolean) => {
+      const allDoors = Array.from({ length: 24 }, (_, i) => i + 1);
+      return showAll ? allDoors : [...state.persistedOpenedDoors];
     },
   },
   persist: true,
